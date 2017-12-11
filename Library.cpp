0e7de320c7eb93044a3caf1c9b7322bbd2526ad5
bkeith <bnkeith.20@gmail.com>
2017-12-11 12:06:07 -0500

    added library class

---
+++ Library.cpp
@@ -0,0 +1,5 @@
+//
+// Created by bnkei on 12/11/2017.
+//
+
+#include "Library.h"
---
+++ Library.h
@@ -0,0 +1,14 @@
+//
+// Created by bnkei on 12/11/2017.
+//
+
+#ifndef INC_220FINALPROJECT_LIBRARY_H
+#define INC_220FINALPROJECT_LIBRARY_H
+
+
+class Library {
+
+};
+
+
+#endif //INC_220FINALPROJECT_LIBRARY_H
