5efb729e2eddb0b90e1a942918f808b6f5f9e55e
bkeith <bnkeith.20@gmail.com>
2017-12-12 09:31:51 -0500

    working on library

--- Library.cpp
+++ Library.cpp
@@ -3,3 +3,17 @@
 //
 
 #include "Library.h"
+#include "List.h"
+#include "ArrayList.h"
+
+Library::Library() {
+    this->list = new ArrayList<Book*>();
+}
+
+Library::~Library(){
+    delete list;
+}
+
+void Library::add(Book *toAdd) {
+    
+}
