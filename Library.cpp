b493ba6c7d80289a5067e8a3ff82340334ee3912
bkeith <bnkeith.20@gmail.com>
2017-12-14 23:11:32 -0500

    more debugs

--- Library.cpp
+++ Library.cpp
@@ -22,15 +22,19 @@ Library::~Library(){
 //may want to change later to make recursive for efficiency
 void Library::add(Book *toAdd) {
     int index = 0;
-    while((toAdd->getTitle()>list->getValueAt(index)->getTitle())&&(index<list->itemCount())){
-        index++;
+    if(!list->isEmpty()) {
+        while ((toAdd->getTitle() > list->getValueAt(index)->getTitle()) && (index < list->itemCount())) {
+            index++;
+        }
+        if (toAdd->getTitle() == list->getValueAt(index)->getTitle()) {
+            list->getValueAt(index)->stock(toAdd->getHaveCount());
+            delete toAdd;
+        } else {
+            list->insertAt(toAdd, index);
+        }
     }
-    if(toAdd->getTitle()==list->getValueAt(index)->getTitle()){
-        list->getValueAt(index)->stock(toAdd->getHaveCount());
-        delete toAdd;
-    }
-    else {
-        list->insertAt(toAdd, index);
+    else{
+        list->insertAtFront(toAdd);
     }
 }
 
@@ -47,13 +51,20 @@ Book* Library::accessBook(std::string title) {
 
 void Library::getInfo(std::string title) {
     int index=find(title);
-    std::cout<<list->getValueAt(index)->getTitle()<<std::endl;
-    std::cout<<"by "<<list->getValueAt(index)->getAuthor()<<std::endl;
-    std::cout<<"$"<<list->getValueAt(index)->getPrice()<<std::endl;
-    std::cout<<list->getValueAt(index)->getHaveCount()<<" copies in stock"<<std::endl;
-    std::cout<<list->getValueAt(index)->getWantCount()<<" copies should be in stock"<<std::endl;
-    std::cout<<"Customers waiting for book: "<<list->getValueAt(index)->getWaitList()->getItemCount()<<std::endl;
-    std::cout<<" "<<std::endl;
+    if(index!=-1) {
+        std::cout << list->getValueAt(index)->getTitle() << std::endl;
+        std::cout << "by " << list->getValueAt(index)->getAuthor() << std::endl;
+        std::cout << "$" << list->getValueAt(index)->getPrice() << std::endl;
+        std::cout << list->getValueAt(index)->getHaveCount() << " copies in stock" << std::endl;
+        std::cout << list->getValueAt(index)->getWantCount() << " copies should be in stock" << std::endl;
+        std::cout << "Customers waiting for book: " << list->getValueAt(index)->getWaitList()->getItemCount()
+                  << std::endl;
+        std::cout << " " << std::endl;
+    }
+    else{
+        std::cout<<"Title not in our inventory!"<<std::endl;
+        std::cout<<" "<<std::endl;
+    }
 }
 
 int Library::sell(std::string title) {
@@ -133,11 +144,13 @@ void Library::returnBooks(std::string fileName) {
 
 int Library::find(std::string title) {//probably want to implement binfind later
     int index = 0;
-    while((title>=list->getValueAt(index)->getTitle())&&(index<list->itemCount())){
-        if(title==list->getValueAt(index)->getTitle()){
-            return index;
+    if(!list->isEmpty()) {
+        while ((title >= list->getValueAt(index)->getTitle()) && (index < list->itemCount())) {
+            if (title == list->getValueAt(index)->getTitle()) {
+                return index;
+            }
+            index++;
         }
-        index++;
     }
     return -1;
 }
